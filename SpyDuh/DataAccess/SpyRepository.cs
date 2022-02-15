c013c224773c9ed581b2cfb05354a3a3e1095424
albertchitta <albert.w.chittaphong@gmail.com>
2022-02-14 19:23:35 -0600

    Added test spy

--- SpyDuh/DataAccess/SpyRepository.cs
+++ SpyDuh/DataAccess/SpyRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace SpyDuh.DataAccess
+﻿using SpyDuh.Models;
+
+namespace SpyDuh.DataAccess
 {
     public class SpyRepository
     {
+        private static List<Spies> _spies = new List<Spies>()
+        {
+            new Spy()
+            {
+                CodeName = "albert",
+                OriginStory = "revenge",
+            }
+        };
+
+        internal object GetByName(string name)
+        {
+            var match = _spies.FirstOrDefault(s => s.CodeName == name);
+            return match;
+        }
+
+        internal List<Spy> GetAll()
+        {
+            return _spies;
+        }
     }
 }
