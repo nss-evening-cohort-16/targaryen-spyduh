1c53a1a6ca7630a9af4ec6c5c7a4901a15ac8758
gabriel smith <damngabe1@gmail.com>
2022-02-14 19:13:54 -0600

    test spy

--- SpyDuh/DataAccess/SpyRepository.cs
+++ SpyDuh/DataAccess/SpyRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace SpyDuh.DataAccess
+﻿using SpyDuh.Models;
+
+namespace SpyDuh.DataAccess
 {
     public class SpyRepository
     {
+        private static List<Spies> _spies = new List<Spies>()
+        {
+            new Spies()
+            {
+                CodeName = "Ivan",
+                SkillsAndServices = new Dictionary<string, double>
+                {
+                    {
+                        "Stealth",
+                        1000.99
+                    }
+                },
+                OriginStory = "From Soviet Era Russia",
+            }
+        };
     }
 }
